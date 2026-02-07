be75aa18e06ed2e369b2678614b6cf97a9b9488a
Margarita <gorbashevarira@gmail.com>
2026-02-07 10:20:14 +0300

    Use a data class

--- src/Question.kt
+++ src/Question.kt
@@ -1,4 +1,4 @@
-class Question<T>(
+data class Question<T>(
     val questionText: String,
     val answer: T,
     val difficulty: Difficulty
@@ -29,4 +29,6 @@ fun main() {
                 println(question1.answer)
                 println(question2.answer)
                 println(question3.answer)
+
+    println(question1.toString())
 }
