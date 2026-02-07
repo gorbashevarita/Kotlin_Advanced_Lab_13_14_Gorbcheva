91c6fb44cbe21198cadc46f09590e4238136e8bf
Margarita <gorbashevarira@gmail.com>
2026-02-07 21:18:30 +0300

    groupBy() function

--- src/Cookie.kt
+++ src/Cookie.kt
@@ -52,13 +52,26 @@ fun main() {
 //    cookies.forEach {
 //        println("Пункт меню: ${it.name}")
 //    }
-    val softBakedMenu = cookies.filter {
-        it.softBaked
-    }
-    println("Мягкое печенье:")
+//    val softBakedMenu = cookies.filter {
+//        it.softBaked
+//    }
+//    println("Мягкое печенье:")
+//    softBakedMenu.forEach {
+//        println("${it.name} - ${it.price}")
+//    }
+
+    val groupedMenu = cookies.groupBy { it.softBaked }
+    val softBakedMenu = groupedMenu[true] ?: emptyList()
+    val crunchyMenu = groupedMenu[false] ?: emptyList()
+
+    println("Soft cookies:")
     softBakedMenu.forEach {
-        println("${it.name} - ${it.price}")
+        println("${it.name} - $$${it.price}")
     }
 
+    println("Crunchy cookies:")
+    crunchyMenu.forEach {
+        println("${it.name} - $$${it.price}")
+    }
 
 }
